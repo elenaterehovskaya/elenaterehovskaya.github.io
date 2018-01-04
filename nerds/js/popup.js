86d1694c82f40afe2314bb0bac0c459ec6748597
elenaterehovskaya <len-sh@yandex.ru>
2018-01-04 19:50:32 +0300

    Портфолио — «Нёрдс» js

--- nerds/js/popup.js
+++ nerds/js/popup.js
@@ -3,6 +3,7 @@ var link = document.querySelector(".button-writeus");
 var popup = document.querySelector(".modal-writeus");
 var close = popup.querySelector(".modal-close");
 
+var form = popup.querySelector("form");
 var username = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=email]");
 
@@ -23,12 +24,25 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("username", username.value);
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
   }
 });
